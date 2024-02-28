5c6054253b1c7fbe8620f81f0275b9abeca60775
David Orona <davidorona2@gmail.com>
2024-02-27 19:40:20 -0800

    Created space invader project

---
+++ Space Invaders/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public delegate void EnemyDied(int pointsWorth);
+    public static event EnemyDied OnEnemyDied;
+
+    public int points = 10;
+
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Ouch!");
+        Destroy(collision.gameObject);
+
+        OnEnemyDied.Invoke(points);
+        Destroy(gameObject);
+    }
+}
