20899bf2545913894a2e683db0414b41f323a9fb
Salverz <davidorona2@gmail.com>
2024-03-20 23:25:48 -0700

    Finished tower defense pt 1

---
+++ Tower Defense/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+    private GameObject turretToBuild;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one BuildManager in scene!");
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+
+    void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ Tower Defense/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+    public TextMeshProUGUI waveCountdownText;
+    private int waveNumber = 0;
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+        waveCountdownText.text = Mathf.Ceil(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveNumber++;
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+        Debug.Log("Wave Incoming!");
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
